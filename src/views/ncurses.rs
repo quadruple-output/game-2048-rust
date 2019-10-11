d7553fb19faf7548271df2523d4739878b6540ed
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2019-10-11 23:32:25 +0200

    some refactoring of ncurses view

--- src/views/ncurses.rs
+++ src/views/ncurses.rs
@@ -3,7 +3,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use super::View;
-use crate::game::{Board, Game, GameState, Square};
+use crate::game::{Game, Square};
 
 // NCurses HOWTO: http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/
 // man pages: man 3x <function>
@@ -15,33 +15,26 @@ pub struct NCursesView {
 
 impl View for NCursesView {
 	fn update(&self) {
-		let game = self.game.borrow();
-		let (mut screen_height, mut screen_width) = (0, 0);
-		let (mut win_height, mut win_width);
 		nc::erase(); // like clear(), but without implicit refresh()
-		nc::getmaxyx(nc::stdscr(), &mut screen_height, &mut screen_width);
-		win_height = screen_height - 4;
-		win_width = screen_width - 4;
-		// calculate height of window for optimal symmetry (height-2)%game.size == 0
-		win_height = win_height - (win_height - 2) % game.board.size_y() as i32; // -2 for borders
-		win_width = win_width - (win_width - 2) % game.board.size_x() as i32;
-		let board_win = nc::subwin(nc::stdscr(), win_height, win_width, 2, 2);
-		if let GameState::Over = game.state() {
-			nc::wattr_on(board_win, nc::A_BLINK());
-		}
-		nc::box_(board_win, 0, 0);
-		nc::wattr_off(board_win, nc::A_BLINK());
-		if let GameState::Over = game.state() {
-			nc::wattr_on(board_win, nc::A_STANDOUT());
-		}
-		self.show_board_in_window(&game.board, board_win);
-		nc::wattr_off(board_win, nc::A_STANDOUT());
+		let board_box_window = self.fit_board_box_in(nc::stdscr());
+		// 		if let GameState::Over = game.state() {
+		// 			nc::wattr_on(board_win, nc::A_BLINK());
+		// 		}
+		let board_window = self.boxed_subwindow(board_box_window);
+		// 		nc::wattr_off(board_win, nc::A_BLINK());
+		// 		if let GameState::Over = game.state() {
+		// 			nc::wattr_on(board_win, nc::A_STANDOUT());
+		// 		}
+		self.show_board_in_window(board_window);
+		// 		nc::wattr_off(board_win, nc::A_STANDOUT());
 		nc::refresh();
-		nc::delwin(board_win);
+		nc::delwin(board_window);
 	}
 }
 
 impl NCursesView {
+	const BORDER_WIDTH: i32 = 1;
+
 	pub fn new(game: Rc<RefCell<Game>>) -> Self {
 		nc::initscr();
 		nc::curs_set(nc::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
@@ -49,26 +42,67 @@ impl NCursesView {
 		NCursesView { game }
 	}
 
-	fn show_board_in_window(&self, board: &Board, window: nc::WINDOW) {
+	fn fit_board_box_in(&self, window: nc::WINDOW) -> nc::WINDOW {
+		let (mut screen_height, mut screen_width) = (0, 0);
+		nc::getmaxyx(window, &mut screen_height, &mut screen_width);
+		// leave room for outer box:
+		screen_height -= 2 * Self::BORDER_WIDTH;
+		screen_width -= 2 * Self::BORDER_WIDTH;
+		// calc optimal coords for board:
+		let (squares_height, squares_width) = self.calc_optimal_board_win(screen_height, screen_width);
+		nc::derwin(window, squares_height + 2 * Self::BORDER_WIDTH, squares_width + 2 * Self::BORDER_WIDTH, 0, 0)
+	}
+
+	fn boxed_subwindow(&self, window: nc::WINDOW) -> nc::WINDOW {
+		let (mut win_height, mut win_width) = (0, 0);
+		nc::getmaxyx(window, &mut win_height, &mut win_width);
+		nc::box_(window, 0, 0);
+		nc::derwin(
+		           window,
+		           win_height - 2 * Self::BORDER_WIDTH,
+		           win_width - 2 * Self::BORDER_WIDTH,
+		           Self::BORDER_WIDTH,
+		           Self::BORDER_WIDTH
+		)
+	}
+
+	fn calc_optimal_board_win(&self, max_height: i32, max_width: i32) -> (i32, i32) {
+		let board = &self.game.borrow().board;
+		// calculate dimensions such that dimension % game.size == 0
+		let mut height_mod_game_size = max_height - (max_height % board.size_y() as i32);
+		let width_mod_game_size = max_width - (max_width % board.size_x() as i32);
+		// height must be a multiple of an odd number, in order to vertically center the
+		// label:
+		if height_mod_game_size % 2 == 0 {
+			height_mod_game_size -= board.size_y() as i32;
+		}
+		// minimum height is one row per square:
+		if height_mod_game_size < board.size_y() as i32 {
+			height_mod_game_size = board.size_y() as i32;
+		}
+		(height_mod_game_size, width_mod_game_size)
+	}
+
+	fn show_board_in_window(&self, window: nc::WINDOW) {
+		let board = &self.game.borrow().board;
+
 		let mut win_width = 0;
 		let mut win_height = 0;
 		nc::getmaxyx(window, &mut win_height, &mut win_width);
-		win_height -= 2; // -2 chars for border
-		win_width -= 2; // -2 chars for border
 		let size_x = board.size_x();
 		let size_y = board.size_y();
 		for x in 0..size_x {
 			for y in 0..size_y {
-				nc::wmove(
-				          window,
-				          1 + ((y as i32 * win_height + win_height / 2) / size_y as i32),
-				          1 + ((x as i32 * win_width + win_width / 2) / size_x as i32)
-				);
 				let label = match board.at_xy(x, y) {
 					Square::Empty => String::new(),
 					Square::Value(value) => value.to_string()
 				};
-				nc::waddstr(window, &label);
+				nc::mvwaddstr(
+				              window,
+				              (y as i32 * win_height + win_height / 2) / size_y as i32,
+				              (x as i32 * win_width + win_width / 2) / size_x as i32,
+				              &label
+				);
 			}
 		}
 	}
