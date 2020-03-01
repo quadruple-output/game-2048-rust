c4f5a51ea00019c32bc617645efad30039f59795
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2020-03-01 22:03:19 +0100

    Cursor now owns the moves

--- src/game/board/cursor.rs
+++ src/game/board/cursor.rs
@@ -8,71 +8,78 @@ pub struct DualCursor<'a> {
 	source:         Coord,
 	target_changed: bool,
 	board:          &'a mut Board,
-	direction:      Vector
+	direction:      Vector,
+	moves:          Vec<Move>
 }
 
 impl<'a> DualCursor<'a> {
 	pub fn new(board: &'a mut Board, start: Coord, direction: Vector) -> Self {
-		Self { target: start, source: start.add(direction).unwrap(), board, direction, target_changed: false }
-	}
-
-	pub fn advance_both(&mut self, moves: &mut Vec<Move>) -> Result<(), ()> {
-		self.advance_source(moves)?; // order matters as advance_target() may also implicitly advance source if they are adjacent
-		self.advance_target(moves)
-	}
-
-	pub fn advance_source(&mut self, moves: &mut Vec<Move>) -> Result<(), ()> {
-		match self.source.add(self.direction) {
-			Ok(coord) => {
-				self.source = coord;
-				Ok(())
-			},
-			Err(()) => {
-				self.push_unchanged_target(moves);
-				Err(())
-			}
-		}
-	}
-	pub fn advance_target(&mut self, moves: &mut Vec<Move>) -> Result<(), ()> {
-		self.push_unchanged_target(moves);
-		self.target = self.target.add(self.direction).unwrap(); // target always smaller than source => cannot fail
-		self.target_changed = false;
-		if self.target == self.source {
-			// source and target must not point to the same coord
-			self.advance_source(moves)
-		} else {
-			Ok(())
-		}
+		Self { target: start,
+		       source: start.add(direction).unwrap(),
+		       board,
+		       direction,
+		       target_changed: false,
+		       moves: vec![] }
 	}
 
 	pub fn source_tile(&self) -> Square { self.board.at(self.source) }
 
 	pub fn target_tile(&self) -> Square { self.board.at(self.target) }
 
-	pub fn move_tile(&mut self, tile_value: TileValue, moves: &mut Vec<Move>) -> () {
+	pub fn moves(self) -> Vec<Move> { self.moves }
+
+	pub fn advance_both(&mut self) -> Result<(), ()> {
+		self.advance_source()?; // order matters as advance_target() may also implicitly advance source if they are adjacent
+		self.advance_target()
+	}
+
+	pub fn advance_source(&mut self) -> Result<(), ()> {
+		match self.source.add(self.direction) {
+			Ok(coord) => {
+				self.source = coord;
+				Ok(())
+			},
+			Err(()) => {
+				// reached the end of the board => collect final Move::Stay if applicable
+				self.push_unchanged_target();
+				Err(())
+			}
+		}
+	}
+
+	pub fn advance_target(&mut self) -> Result<(), ()> {
+		self.push_unchanged_target();
+		self.target = self.target.add(self.direction).unwrap(); // target always smaller than source => cannot fail
+		self.target_changed = false;
+		if self.target == self.source {
+			// source and target must not point to the same coord
+			self.advance_source() // can fail
+		} else {
+			Ok(())
+		}
+	}
+
+	pub fn move_tile(&mut self, tile_value: TileValue) -> () {
 		self.board.put(self.target, Value(tile_value));
 		self.board.put(self.source, Empty);
 		self.target_changed = true;
-		moves.push(Move::Shift { from: self.source, to: self.target, value: tile_value });
+		self.moves.push(Move::Shift { from: self.source, to: self.target, value: tile_value });
 	}
 
-	pub fn merge_tiles(&mut self, old_tile_value: TileValue, new_tile_value: TileValue,
-	                   moves: &mut Vec<Move>)
-	                   -> ()
-	{
+	pub fn merge_tiles(&mut self, old_tile_value: TileValue, new_tile_value: TileValue) -> () {
 		self.board.put(self.target, Value(new_tile_value));
 		self.board.put(self.source, Empty);
 		self.target_changed = true;
-		moves.push(Move::Merge { from:        self.source,
-		                         to:          self.target,
-		                         start_value: old_tile_value,
-		                         end_value:   new_tile_value });
+		self.moves.push(Move::Merge { from:        self.source,
+		                              to:          self.target,
+		                              start_value: old_tile_value,
+		                              end_value:   new_tile_value });
 	}
 
-	fn push_unchanged_target(&self, moves: &mut Vec<Move>) -> () {
+	fn push_unchanged_target(&mut self) -> () {
 		if !self.target_changed {
 			if let Value(target_value) = self.board.at(self.target) {
-				moves.push(Move::Stay { at: self.target, value: target_value });
+				self.moves.push(Move::Stay { at: self.target, value: target_value });
 			}
 		}
 	}
--- src/game/board/merger.rs
+++ src/game/board/merger.rs
@@ -9,29 +9,28 @@ pub struct Merger<'a> {
 impl<'a> Merger<'a> {
 	pub fn new(cursor: DualCursor<'a>) -> Self { Merger { cursor } }
 
-	pub fn merge(self) -> Vec<Move> {
-		let mut moves = Vec::new();
-		self.merge_until_err(&mut moves).err(); // just ignore the result
-		moves
+	pub fn merge(mut self) -> Vec<Move> {
+		self.merge_until_err().err(); // just ignore the result
+		self.cursor.moves()
 	}
 
-	fn merge_until_err(mut self, moves: &mut Vec<Move>) -> Result<(), ()> {
+	fn merge_until_err(&mut self) -> Result<(), ()> {
 		loop {
 			match self.cursor.source_tile() {
 				Empty => {
-					self.cursor.advance_source(moves)?;
+					self.cursor.advance_source()?;
 				},
 				Value(source_value) => match self.cursor.target_tile() {
 					Empty => {
-						self.cursor.move_tile(source_value, moves);
-						self.cursor.advance_source(moves)?;
+						self.cursor.move_tile(source_value);
+						self.cursor.advance_source()?;
 					},
 					Value(target_value) =>
 						if target_value == source_value {
-							self.cursor.merge_tiles(source_value, source_value + target_value, moves);
-							self.cursor.advance_both(moves)?;
+							self.cursor.merge_tiles(source_value, source_value + target_value);
+							self.cursor.advance_both()?;
 						} else {
-							self.cursor.advance_target(moves)?;
+							self.cursor.advance_target()?;
 						},
 				}
 			}
